787d01c9f2f255846824b7cb7f8c6e5a9ce907ba
VytautasPliadis <vytautas.pliadis@gmail.com>
2023-08-22 16:44:55 +0300

    Adding list.py

---
+++ lists.py
@@ -0,0 +1,10 @@
+class ListModifier:
+    def __init__(self):
+        self.arr = []
+
+    def insert(self, x, y):
+        self.arr.insert(x, y)
+
+
+if __name__ == '__main__':
+    N = int(input())
