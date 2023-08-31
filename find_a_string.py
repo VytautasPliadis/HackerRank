83f49e2b5a73388662ccef13349439fdf5b09c08
VytautasPliadis <vytautas.pliadis@gmail.com>
2023-08-31 14:41:00 +0300

    rewriten code in oop

--- find_a_string.py
+++ find_a_string.py
@@ -1,20 +1,25 @@
-def count_substring(string, sub_string):
-    if not 1 <= len(string) <= 200:
-        raise ValueError('String is too big or to small. Max len 200')
+class FindSting:
+    def __init__(self, string, sub_string):
+        self.string = string
+        self.sub_string = sub_string
 
-    count = 0
-    index = string.find(sub_string)
+    def count_substring(string, sub_string):
+        if not 1 <= len(string) <= 200:
+            raise ValueError('String is too big or to small. Max len 200')
 
-    while index != -1:
-        count += 1
-        index = string.find(sub_string, index + 1)
+        count = 0
+        index = string.find(sub_string)
 
-    return count
+        while index != -1:
+            count += 1
+            index = string.find(sub_string, index + 1)
+
+        return count
 
 
 if __name__ == '__main__':
     string = input().strip()
     sub_string = input().strip()
 
-    count = count_substring(string, sub_string)
+    count = FindSting(string, sub_string)
     print(count)
