f8879b1bb8d5859655f63c3cdcd45bedca8717e7
VytautasPliadis <vytautas.pliadis@gmail.com>
2023-08-31 17:05:42 +0300

    fixed class error

--- find_a_string.py
+++ find_a_string.py
@@ -1,18 +1,18 @@
 class FindSting:
-    def __init__(self, string, sub_string):
-        self.string = string
-        self.sub_string = sub_string
+    def __init__(self, text_string, text_sub_string):
+        self.string = text_string
+        self.sub_string = text_sub_string
 
-    def count_substring(string, sub_string):
-        if not 1 <= len(string) <= 200:
+    def count_substring(self):
+        if not 1 <= len(self.string) <= 200:
             raise ValueError('String is too big or to small. Max len 200')
 
         count = 0
-        index = string.find(sub_string)
+        index = self.string.find(self.sub_string)
 
         while index != -1:
             count += 1
-            index = string.find(sub_string, index + 1)
+            index = self.string.find(self.sub_string, index + 1)
 
         return count
 
@@ -21,5 +21,5 @@ if __name__ == '__main__':
     string = input().strip()
     sub_string = input().strip()
 
-    count = FindSting(string, sub_string)
-    print(count)
+    result = FindSting(string, sub_string).count_substring()
+    print(result)
