a7d8ad1d5e866614923557f81d0b27525d92c457
VytautasPliadis <vytautas.pliadis@gmail.com>
2023-09-02 09:40:58 +0300

    fixed some error

--- string_validators.py
+++ string_validators.py
@@ -3,12 +3,11 @@ class StrValidator:
         self.input_list = list(input_list.strip())
         if len(self.input_list) > 1000:
             raise ValueError('Max 1000 chars')
-        self.input_list = list(input_string.strip())
         self.alphanumeric = any(char.isalnum() for char in self.input_list)
         self.letters = any(char.isalpha() for char in self.input_list)
         self.decimals = any(char.isdecimal() for char in self.input_list)
         self.lower_char = any(char.islower() for char in self.input_list)
-        self.upper_char = any(char.isupper() for char in self.iginput_list)
+        self.upper_char = any(char.isupper() for char in self.input_list)
 
     def __str__(self):
         return f'{self.alphanumeric}\n{self.letters}\n{self.decimals}\n{self.lower_char}\n{self.upper_char}'
