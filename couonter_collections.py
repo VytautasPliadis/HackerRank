0804a6592d66620d5a31a4d9e8c487ff64a1113d
VytautasPliadis <vytautas.pliadis@gmail.com>
2023-09-22 16:02:28 +0300

    adding counter_collections.py

--- couonter_collections.py
+++ couonter_collections.py
@@ -1,7 +1,15 @@
 from collections import Counter
 
-myList = [1, 1, 2, 3, 4, 5, 3, 2, 3, 4, 2, 1, 2, 3]
-Counter(myList)
-Counter(myList).items()
-Counter(myList).keys()
-Counter(myList).values()
+# myList = [1, 1, 2, 3, 4, 5, 3, 2, 3, 4, 2, 1, 2, 3]
+# Counter(myList)
+# Counter(myList).items()
+# Counter(myList).keys()
+# Counter(myList).values()
+
+
+if __name__ == '__main__':
+    shoes_quantity = int(input())
+    shoes_list = input()
+    customers_number = int(input())
+
+    shoes_dict = Counter(shoes_list)
